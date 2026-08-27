16ca026011129582f15e6a163461cef1b06557c3
Ben Stone <stone.bt24@gmail.com>
2026-08-27 10:37:47 -0400

    Added Footer section to HTML & CSS. Updated Year sorting in JS. Updated Movies section in CSS.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // API: https://www.omdbapi.com/?i=tt3896198&apikey=1c6a2726&s=(keyword)
 
 let movies;
-let searchKeyword = '';
+let searchKeyword = "";
 
 async function renderMovies(filter) {
   const moviesWrapper = document.querySelector(".movies");
@@ -17,14 +17,14 @@ async function renderMovies(filter) {
   } else if (filter === "ALPHA_Z_A") {
     movies.sort((a, b) => b.Title.localeCompare(a.Title));
   } else if (filter === "NEW_OLD") {
-    movies.sort((a, b) => b.Year - a.Year);
+    movies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
   } else if (filter === "OLD_NEW") {
-    movies.sort((a, b) => a.Year - b.Year);
+    movies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
   }
 
-  const moviesHTML = movies.map((movie) => {
-    // const poster = movie.Poster !== "N/A" ? movie.Poster : "./assets/movie_logo.jpg";
-    return `<div class="movie">
+  const moviesHTML = movies
+    .map((movie) => {
+      return `<div class="movie">
       <figure class="movie__img--wrapper">
         <img class="movie__img" src="${movie.Poster}" alt=""
         onerror="this.onerror=null; this.src='./assets/movie_logo.jpg';">
@@ -34,23 +34,22 @@ async function renderMovies(filter) {
         <h4 class="movie__year">${movie.Year}</h4>
       </div>
     </div>`;
-  })
-  .join('');
+    })
+    .join("");
 
   moviesWrapper.innerHTML = moviesHTML;
 }
 
 async function getMovies(keyword) {
   const moviesList = await fetch(
-    `https://www.omdbapi.com/?i=tt3896198&apikey=1c6a2726&s=${keyword}`,
-  );
+    `https://www.omdbapi.com/?i=tt3896198&apikey=1c6a2726&s=${keyword}`);
   const movieData = await moviesList.json();
   return movieData.Search || [];
 }
 
 function onSearchChange(event) {
   searchKeyword = event.target.value;
-  renderMovies(document.querySelector('#filter').value);
+  renderMovies(document.querySelector("#filter").value);
 }
 
 function filterMovies(event) {
@@ -62,5 +61,6 @@ renderMovies();
 //To target a string inside an object that will be used for sorting:
 //1. target the variable we want to sort (movies) followed by the sort property (.sort(() => ))
 //2. in the first parentheses add two parameters (a, b)
-//3.  
-
+//3. after the arrow function (=>) add the first parameter (a) followed by a dot followed by the object element you want to target (a.Title) - this order of parameters is following asending order, to follow descending order flip a and b after the arrow function
+//4. in the second parentheses do the same as step 3 but for the second paramter (b)
+//5. after the first parameter/target add a dot followed by localeCompare which is followed by the second parentheses (a.Title.localeCompare(b.Title)) which will sort/compare the first string/parameter with the second just like (a.Year - b.Year) does for numbers
